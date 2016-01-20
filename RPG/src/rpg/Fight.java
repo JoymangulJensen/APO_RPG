9e60f21e9c0e0fb4c35e7271167d218bd7411874
JoymangulJensen <jensen.joymangul@etu.univ-lyon1.fr>
2016-01-20 13:49:19 +0100

    add sword ascii art

--- RPG/src/rpg/Fight.java
+++ RPG/src/rpg/Fight.java
@@ -49,6 +49,15 @@ public class Fight extends Event
     @Override
     public void launch()
     {
+        System.out.println("              (O)");
+        System.out.println("              <M");
+        System.out.println("   o          <M");
+        System.out.println("  /| ......  /:M\\------------------------------------------------,,,,,,");
+        System.out.println("(O)[]XXXXXX[]I:K+}=====<{H}>================================------------>");
+        System.out.println("  \\| ^^^^^^  \\:W/------------------------------------------------''''''");
+        System.out.println("   o          <W");
+        System.out.println("              <W");
+        System.out.println("              (O)" + Main.ANSI_RESET);
         System.out.println(introMessage);
         this.fight();
     }
